ae6d1faa264eadd56ac4be1e36d94c9725c0e671
Le Si Bich <moonlight8978@gmail.com>
2017-04-23 20:30:05 +0700

    update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,19 +5,49 @@ class ArticlesController < ApplicationController
 
 	def show
     @article = Article.find(params[:id])
+    # render plain: @article.inspect
   end
- 
+
   def new
+    @article = Article.new
   end
 
   def create
     @article = Article.new(article_params)
 
-    @article.save
-    redirect_to @article
-    # redirect_to(controller: 'articles', action: 'show', id: @article.id)
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+    # redirect_to(
+    #   controller: :articles,
+    #   action: :show,
+    #   id: @article.id
+    # )
   end
- 
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
+  end
+
   private
     def article_params
       params.require(:article).permit(:title, :text)
