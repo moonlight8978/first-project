6ce730a1dfde19adc7bdef63de0e3e269bfa79e7
Le Si Bich <moonlight8978@gmail.com>
2017-04-24 21:57:33 +0700

    add vendor

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
+
+//= require jquery/dist/jquery.min
 //= require jquery_ujs
 //= require turbolinks
-//= require_tree .
+//= require tether/dist/js/tether.min
+//= require bootstrap/dist/js/bootstrap.min
+//= require flux-slider/js/flux.min
+//= require_tree
+
---
+++ app/assets/javascripts/welcome.js
@@ -0,0 +1,1 @@
+$("#hint a").tooltip()
