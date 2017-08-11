58319f8d3b0ee96ffbef396e549c467f706d09d1
_MoonLight_ <moonlight8978@gmail.com>
2017-08-11 18:40:57 +0700

    remove client from master

--- client/archived/app/constants.js
+++
@@ -1,6 +0,0 @@
-export constant SERVER = {
-    DOMAIN  = 'http://localhost:3000';
-    API     = 'http://localhost:3000/api';
-    LOGIN   = 'http:localhost:3000/login';
-    LOGOUT  = 'http:localhost:3000/logout';
-};
--- client/gulp/config.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = {
-    app: 'public/',
-    bower: 'public/bower_components/',
-    assets_path: 'public/assets/',
-	  js_path: 'public/app/',
-	  build: 'public/build/'
-};
